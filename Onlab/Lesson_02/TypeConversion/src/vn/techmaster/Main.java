8641edec876b2d2c876856adc6ae3594cbf0fed3
EriChannel <erichannel96@gmail.com>
2021-10-14 21:59:15 +0700

    Buổi 2

---
+++ Onlab/Lesson_02/TypeConversion/src/vn/techmaster/Main.java
@@ -0,0 +1,15 @@
+package vn.techmaster;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Calculator cal = new Calculator();
+//        double result = cal.div(9, 2);
+//        System.out.println("Kết quả: " + result);
+
+        double a = 8.5;
+        int b = (int)a;
+        System.out.println("Biến a sau khi ép kiểu: " +b);
+
+    }
+}
