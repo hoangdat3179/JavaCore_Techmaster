7b0ac1ff07cae44fc8b63a6b0a98dc6823368e90
EriChannel <erichannel96@gmail.com>
2021-10-20 09:39:26 +0700

    Buổi 4

--- Onlab/Lesson_03/exercise/src/vn/techmaster/Main.java
+++ Onlab/Lesson_03/exercise/src/vn/techmaster/Main.java
@@ -10,9 +10,44 @@ public class Main {
         System.out.print("Nhập y: ");
         int y = sc.nextInt();
 
-        System.out.println("x+y = " +Calculator.addition(x,y));
-        System.out.println("x-y = " +Calculator.subtraction(x,y));
-        System.out.println("x*y = " +Calculator.multiplication(x,y));
-        System.out.println("x/y = " +Calculator.division(x,y));
+        menu();
+        int choice = sc.nextInt();
+
+        switch (choice){
+            case 1:
+                System.out.println("x+y = " +Calculator.addition(x,y));
+                break;
+            case 2:
+                System.out.println("x-y = " +Calculator.subtraction(x,y));
+                break;
+            case 3:
+                System.out.println("x*y = " +Calculator.multiplication(x,y));
+                break;
+            case 4:
+                System.out.println("x/y = " +Calculator.division(x,y));
+                break;
+            case 0:
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Không có lựa chọn này!!!");
+
+        }
+
+
+
+
+
+
+    }
+
+    public static void menu(){
+        System.out.println("Mời bạn lựa chọn: ");
+        System.out.println("1 - x+y");
+        System.out.println("2 - x-y");
+        System.out.println("3 - x*y");
+        System.out.println("4 - x/y");
+        System.out.println("0 - Thoát chương trình");
+        System.out.println("Lựa chọn của bạn là: ");
     }
 }
