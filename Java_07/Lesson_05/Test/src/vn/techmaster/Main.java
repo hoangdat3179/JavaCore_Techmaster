2111dc052988b0b254dbafbb5dabc4aa1727e11e
Lập trình cùng Eri <erichannel96@gmail.com>
2021-11-29 16:37:25 +0700

    Chữa bài tập

---
+++ Java_07/Lesson_05/Test/src/vn/techmaster/Main.java
@@ -0,0 +1,32 @@
+package vn.techmaster;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Bài 1
+//        Exercise_01.drawSquare();
+//        Exercise_01.drawRightTriangle();
+//        Exercise_01.drawEquilateralTriangle();
+
+        //Bài 2
+//        String str = "You only live once, but if you do it right, once is enough";
+//        System.out.println("Số từ có trong chuỗi: " + Exercise_02.countWord(str));
+//        Exercise_02.countCharater(str);
+
+        //Bài 3
+//        Exercise_03.randomNumber();
+
+        //Bài 4
+//        Exercise_04.printPrimeNumberLessThan10(10);
+//        Exercise_04.printFirst10PrimeNumber(10);
+
+        //Bài 5
+//        Exercise_05.standardizeString();
+//        Exercise_05.checkPalindrome();
+
+        //Bài 6
+        int[][] arr = Exercise_06.createArray();
+        Exercise_06.printArray(arr);
+        Exercise_06.findDiagonalLine(arr);
+    }
+}
