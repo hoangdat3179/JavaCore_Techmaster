3434861942999a18598f502bf24cdf96b9e93b0d
Lập trình cùng Eri <erichannel96@gmail.com>
2021-12-06 20:49:17 +0700

    Buổi 8

---
+++ Java_07/Lesson_08/Calculator/src/vn/techmaster/Main.java
@@ -0,0 +1,13 @@
+package vn.techmaster;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Calculator.sum(5,6);
+        System.out.println("");
+        Calculator.sum(5,6,7);
+        System.out.println();
+        Calculator.sum(5.5, 6.6);
+    }
+}
---
+++ Java_07/Lesson_08/Person/src/vn/techmaster/Main.java
@@ -0,0 +1,13 @@
+package vn.techmaster;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Student s = new Student("Ngọc", LocalDate.of(1996, 8,19), "HN", 9);
+        System.out.println(s);
+
+
+    }
+}
